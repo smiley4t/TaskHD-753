79271ff20e9c47d037b407964217cd6f48dbd352
Thao Tran <48933961+smiley4t@users.noreply.github.com>
2024-09-24 19:50:30 +1000

    Create eslint.config.js

---
+++ eslint.config.js
@@ -0,0 +1,22 @@
+
+module.exports = {
+    env: {
+        browser: true, // Browser global variables
+        es2021: true,  // ECMAScript 2021 features
+        node: true,    // Node.js global variables and Node.js scoping
+    },
+    extends: [
+        'eslint:recommended', // Use the recommended rules from ESLint
+    ],
+    parserOptions: {
+        ecmaVersion: 12,      // Allow modern ECMAScript features
+        sourceType: 'module', // Allows using imports
+    },
+    rules: {
+        'linebreak-style': ['error', 'unix'], // Linebreak style
+        'quotes': ['error', 'single'],         // Single quotes
+        'semi': ['error', 'always'],           // Always use semicolons
+        'no-unused-vars': ['warn'],             // Warn on unused variables
+        'no-console': 'off',                     // Allow console statements (or change to 'warn')
+    },
+};
