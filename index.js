fea941ac711cb726321e9e869e3e609c9d51dbce
Thao Tran <48933961+smiley4t@users.noreply.github.com>
2024-09-25 16:58:39 +1000

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 // Require the express web application framework (https://expressjs.com)
-const express = require('express')
+const express = require('express');
 
 // Create a new web application by calling the express function
-const app = express()
-const port = 3000
+const app = express();
+const port = 3000;
 app.use(express.json()); 
 // Tell our application to serve all the files under the `public_html` directory
-app.use(express.static('public_html'))
+app.use(express.static('public_html'));
 app.post('/feedback', (req, res) => {
   try {
     const { name, feedback } = req.body;
@@ -25,7 +25,7 @@ app.listen(port, ()=> {
   // When the application starts, print to the console that our app is
   // running at http://localhost:3000. Print another message indicating
   // how to shut the server down.
-  console.log(`Web server running at: http://localhost:${port}`)
+  console.log(`Web server running at: http://localhost:${port}`);
   console.log(`Type Ctrl+C to shut down the web server`)
-})
+});
 module.exports = app;
