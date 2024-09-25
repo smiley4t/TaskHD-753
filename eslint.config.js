0409b92724647058e5ecec9f7cdd222322a623fe
Thao Tran <48933961+smiley4t@users.noreply.github.com>
2024-09-25 16:11:31 +1000

    Update eslint.config.js

--- eslint.config.js
+++ eslint.config.js
@@ -1,27 +1,26 @@
 
 module.exports = {
-    files: ['**/*.js'],
-    languageOptions: {
-      // Defining global variables previously declared in the "env" key
-      globals: {
-        node: true, // Node.js environment
-        es6: true,  // ECMAScript 6 globals
-      },
-      // Specify the ECMAScript version to use (you can adjust this based on your project)
-      
-    
-    extends: [
-        'eslint:recommended', // Use the recommended rules from ESLint
-    ],
-    parserOptions: {
-        ecmaVersion: 12,      // Allow modern ECMAScript features
-        sourceType: 'module', // Allows using imports
+  files: ['**/*.js'], // Files to lint
+  languageOptions: {
+    // Defining global variables
+    globals: {
+      node: true, // Node.js environment
+      es6: true,  // ECMAScript 6 globals
     },
-    rules: {
-        'linebreak-style': ['error', 'unix'], // Linebreak style
-        'quotes': ['error', 'single'],         // Single quotes
-        'semi': ['error', 'always'],           // Always use semicolons
-        'no-unused-vars': ['warn'],             // Warn on unused variables
-        'no-console': 'off',                     // Allow console statements (or change to 'warn')
-    },
-}
+    ecmaVersion: 12, // Specify the ECMAScript version
+  },
+  extends: [
+    'eslint:recommended', // Use the recommended rules from ESLint
+  ],
+  parserOptions: {
+    ecmaVersion: 12,      // Allow modern ECMAScript features
+    sourceType: 'module', // Allows using imports
+  },
+  rules: {
+    'linebreak-style': ['error', 'unix'], // Enforce Unix linebreaks
+    'quotes': ['error', 'single'],        // Enforce single quotes
+    'semi': ['error', 'always'],          // Enforce semicolons
+    'no-unused-vars': ['warn'],           // Warn on unused variables
+    'no-console': 'off',                  // Allow console statements
+  },
+};
